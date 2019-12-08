68c8c44a57bf2769bafbddd46c334e767dc548c8
Valien R <valien.r@gmail.com>
2019-12-08 17:05:17 +0300

    init git

--- main.py
+++ main.py
@@ -4,30 +4,35 @@ import json
 import time
 import dht
 from machine import Pin
+import machine
 
 
 def show_message(request):
-    """
-     request handler
-    """
-    html = """<!DOCTYPE html>
+    html = """
+    <!DOCTYPE html>
     <html>
-    <head> <title>ESP8266 Pins</title> </head>
-    <body> <h1>ESP8266 Pins</h1>
+    <body>
+    <center>
+    <h1>WiFi LED on off test: 1</h1><br>
+    Ciclk to show temperature data <a href="data">dht json</a><br>
+    Ciclk to turn <a href="on">LED ON</a><br>
+    Ciclk to turn <a href="off">LED OFF</a><br>
+    <hr>
+    </center>
     </body>
     </html>
     """
-    
     server.send(html, content_type='text/html charset=utf-8')
 
 
 def do_on(request):
-    ''' on request handler '''
+    """ on request handler """
     pin.value(1)
-    server.send("ON") ]
+    server.send("ON")
+
 
 def do_off(request):
-    ''' off request handler '''
+    """ off request handler """
     pin.value(0)
     server.send("OFF")
 
@@ -41,14 +46,13 @@ def show_data(request):
     server.send(json_str, content_type="Content-Type: application/json")
 
 
-
 pin = machine.Pin(13, machine.Pin.OUT)
+
+""" init server """
 server = MicroPyServer()
-''' add request handler '''
 server.add_route("/", show_message)
 server.add_route("/data", show_data)
 server.add_route("/on", do_on)
 server.add_route("/off", do_off)
-''' start server '''
+""" start server """
 server.start()
-
